41a1a57391cba77d1fdd053fcc0b969703190b25
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-10-21 14:39:36 -0600

    Hangman

--- final.c
+++ final.c
@@ -1,40 +1,75 @@
-//RA AB EW EG
-______
-|    |
-|    |
-|    O
-|   /|\
-|   / \
-|_______
-
+//RA AB EW EG;
 
 
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(livy)){
+    
+    if(lives == 0){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|   /|\\ \n|   / \\ |_______\n";
+        scanf("%s", livy);
+    }else if(lives == 1){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|   /|\\ \n|   /  \n|_______\n";
+        scanf("%s", livy);
+    }else if(lives == 2){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|   /|\\ \n|    |_______\n";
+        scanf("%s", livy);
+    }else if(lives == 3){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|   /| \n|   |_______\n";
+        scanf("%s", livy);
+    }else if(lives == 4){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|   / \n|    |_______\n";
+        scanf("%s", livy);
+    }else if(lives == 5){
+        char livy[54] = "_____\n|    |\n|    |\n|    o\n|    \n|    |_______\n";
+        scanf("%s", livy);
+    }else{
+        char livy[54] = "_____\n|    |\n|    |\n|    \n|    \n|    |_______\n";
+        scanf("%s", livy);
+    }
+    return livy;
+}
 
 int main(void){
-    word_list = 
-    word = random.choice(word_list)
-    int lives = 7
-    guessed_letters = []
+	srand(time(NULL));
+	char words[60][30] = {"potato", "wagon", "cooperate", "seek", "sulphur", "center", "bounce", "ruin", "interference", "autonomy", "examination", "roof", "market", "pop", "light", "shock", "skin", "donor", "element", "execution", "vessel", "contrast"};
+	for(int i=0; i<5; i++){
+		int num = rand() % 8;
+		printf("Our random person is %s\n", words[num]);
+    }
 
-    def display_word():
+    char guess[30];l
+    return 0;
+    
+}
+
+
+    int lives = 7;
+
+    char display_word(){
         display = " _ "
 
-        for letter in word:
-            if letter in guessed_letters: 
-                printf(letter, end="") 
-        else:            
-            print(display, end="")
+        for( int i = 0; i < strlen(word); i++){
+            if(guess = word[i]) 
+        }
+            if letter in guessed_letters; 
+                printf(letter, end="");
+            else;         
+                print(display, end="");
     printf(" ")
-
-def did_guess_all_letters():
-
+    }
+int did_guess_all_letters(){
     for letter in word:
         if letter not in guessed_letters:
             return False
     return True
+}
+
+    
 Start of game loop
 
 while(lives >= 1){
@@ -49,19 +84,20 @@ while(lives >= 1){
     printf("|   / \\\n");
     printf("|_______\n");
 
-    if len(guess) > 1:
-        printf("Stop wasting my time.")
-        sys.exit() 
-    guessed_letters.append(guess)
-    if guess in word:
-        printf(f"Correct.")
+    if(guess=false)
+        printf("Stop wasting my time.");
+        sys.exit() ;
+    guessed_letters.append(guess);
+    if s in word:
+        printf(f"Correct.");
     else:
         lives -= 1
-        printf(f"Incorrect.")
+        printf(f"Incorrect.");
     if did_guess_all_letters():
-        printf("Congratulations, you won with %d lives left!", lives)
-        sys.exit()
+        printf("Congratulations, you won with %d lives left!", lives);
+        sys.exit();
     return 0;
-}
+
     printf("You don't have any more guesses, you lose. The word was %s", word);
     sys;exit();
+}
